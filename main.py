f9308318ce999d636d5ee3c3c36b87153018e276
Anonymous <anonymous@noemail.com>
2015-09-27 07:37:45 +0000

    latest

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import webapp2
+from htmlhandler import HtmlHandler
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('<a href="/doc/sutldoc">sUTL doc</a>')
 
 app = webapp2.WSGIApplication([
+    ('/doc/(.*)', HtmlHandler),
     ('/', MainHandler)
 ], debug=True)
